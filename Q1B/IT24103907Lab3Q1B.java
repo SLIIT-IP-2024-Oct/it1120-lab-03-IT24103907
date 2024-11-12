478f6455162beada37441d3a9074bdf0523bde0f
IT24103907 <it24103907@my.sliit.lk>
2024-11-12 09:47:01 -0800

    Lab3Q1B It24103907

---
+++ Q1B/IT24103907Lab3Q1B.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+  public class IT24103907Lab3Q1B {
+  public static void main (String [] args) {
+  Scanner scanner = new Scanner(System.in);
+
+System.out.print("Enter the price of 1Kg of rice: ");
+double pricePerKg = scanner.nextDouble();
+
+System.out.print("Enter the number of kilograms to buy: ");
+double quantity = scanner.nextDouble();
+double totalCost = pricePerKg*quantity;
+double discount = totalCost*0.1;
+double finalAmount = totalCost-discount;
+
+System.out.print("Final amount to pay after discount: "+finalAmount);
+ scanner.close();
+}
+}
